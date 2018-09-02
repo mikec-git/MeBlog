36d31e3467962c39a680d0e62155a55798a30439
Mike C <mike.choi@live.ca>
2018-09-02 21:23:48 +0000

    Initialized package.json and app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// Import dependencies
+var expressSanitizer = require("express-sanitizer"),
+    methodOverride = require("method-override"),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
+
+// Configure middleware
+mongoose.connect("mongodb://localhost/MeBlog", {useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(expressSanitizer());
+
+// Configure database
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//
